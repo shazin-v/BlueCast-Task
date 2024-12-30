ccd9aa48ba136d209cbefb17a56469e0870a2a9a
Shazin v <shazinv.tech@gmail.com>
2024-12-30 14:25:23 +0530

    fixed errors

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import Icon from "../img/icon-person.svg"
 
 const Card = () => {
-  const [amount, setAmount] = useState(0);
-  const [people, setPeople] = useState(0);
-  const [tip, setTip] = useState(0);
-  //   const [error, setError] = useState("");
+  const [amount, setAmount] = useState("");
+  const [people, setPeople] = useState("");
+  const [tip, setTip] = useState("");
+  const [error, setError] = useState("");
 
   console.log(amount, "amount");
   console.log(people, "people");
@@ -13,27 +14,30 @@ const Card = () => {
   const perPersonShare = amount / people || 0;
   console.log("perPersonShare", perPersonShare);
 
-  var tipValue = (tip / 100) * amount;
-  tipValue = Math.round(tipValue * 100) / 100;
-  tipValue = tipValue.toFixed(2);
-  console.log("tipValue", tipValue);
+  const tipValue = (tip / 100) * amount || 0;
+  const totalAmountPerPerson =
+    people > 0 ? (parseFloat(amount) + parseFloat(tipValue)) / people : 0;
+  const tipAmountPerPerson = people > 0 ? tipValue / people : 0;
 
-  //   if (people === 0) {
-  //     setError("Can't be zero");
-  //   }
-  //   console.log("error",error)
-
-  //   const Tip = [
-  //     { id: 1, value: "5" },
-  //     { id: 2, value: "10" },
-  //     { id: 3, value: "15" },
-  //     { id: 4, value: "25" },
-  //     { id: 5, value: "50" },
-  //   ];
+  const handleReset = () => {
+    setAmount("");
+    setPeople("");
+    setTip("");
+    setError("");
+  };
+  const handlePeopleChange = (e) => {
+    const value = e.target.value;
+    setPeople(value);
+    if (value <= 0) {
+      setError("Can't be zero");
+    } else {
+      setError("");
+    }
+  };
 
   return (
     <>
-      <div className="bg-white rounded-lg shadow-md  md:max-w-2xl md:flex p-5">
+      <div className="bg-white rounded-xl shadow-lg md:w-[800px] flex flex-col md:flex-row p-6">
         {/* left side */}
 
         <div className="w-full md:w-1/2 p-5">
@@ -41,16 +45,19 @@ const Card = () => {
             <label htmlFor="" className="font-bold text-gray-400">
               Bill
             </label>
-            <input
-              type="number"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              className="bg-[#f3f8fb] px-5 py-1 justify-between rounded-lg text-[#a3b4b4]"
-              placeholder="$"
-            />
+            <div className="relative mt-2">
+              <span className="absolute left-4 top-3 text-[#7f9d9f]">$</span>
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                className="bg-[#f3f8fb] w-full px-10 py-3 rounded-md text-[#a3b4b4] font-bold text-right"
+                placeholder="0"
+              />
+            </div>
           </div>
 
-          <div>
+          <div className="flex flex-col">
             <label
               htmlFor=""
               className="text-[#748181] capitalize font-semibold py-5"
@@ -59,107 +66,59 @@ const Card = () => {
             </label>
 
             {/* tip */}
-            <div>
-              {/* {Tip.map((item, index) => {
-{                console.log(Tip[0].value,"tip")
-}                <li key={index}>
-                  <button className="bg-[#00474b] text-white">{item.value}</button>;
-                </li>;
-              })} */}
-              <div className="flex flex-wrap gap-2">
-                <div className="bg-[#00474b] text-white rounded-lg w-1/3 list-none">
-                  <input
-                    type="radio"
-                    id="a5"
-                    name="amount"
-                    value={5}
-                    onChange={(e) => setTip(e.target.value)}
-                    className="bg-[#00474b] text-white rounded-lg w-1/3 list-none"
-                  />
-                  <label htmlFor="a5">$5</label>
-                </div>
-
-                <div className="bg-[#00474b] text-white rounded-lg w-1/3 list-none">
-                  <input
-                    type="radio"
-                    id="a10"
-                    name="amount"
-                    value={10}
-                    onChange={(e) => setTip(e.target.value)}
-                    className="bg-[#00474b] text-white rounded-lg w-1/3 list-none"
-                  />
-                  <label htmlFor="a10">$10</label>
-                </div>
-
-                <div className="bg-[#00474b] text-white rounded-lg w-1/3 list-none">
-                  <input
-                    type="radio"
-                    id="a15"
-                    name="amount"
-                    value={15}
-                    onChange={(e) => setTip(e.target.value)}
-                    className="bg-[#00474b] text-white rounded-lg w-1/3 list-none"
-                  />
-                  <label htmlFor="a15">$15</label>
-                </div>
-                <div className="bg-[#00474b] text-white rounded-lg w-1/3 list-none">
-                  <input
-                    type="radio"
-                    id="a25"
-                    name="amount"
-                    value={25}
-                    onChange={(e) => setTip(e.target.value)}
-                    className="bg-[#00474b] text-white rounded-lg w-1/3 list-none"
-                  />
-                  <label htmlFor="a25">$25</label>
-                </div>
-                <div className="bg-[#00474b] text-white rounded-lg w-1/3 list-none">
-                  <input
-                    type="radio"
-                    id="a50"
-                    name="amount"
-                    value={50}
-                    onChange={(e) => setTip(e.target.value)}
-                    className="bg-[#00474b] text-white rounded-lg w-1/3 list-none"
-                  />
-                  <label htmlFor="a50">$50</label>
-                </div>
-                {/* custom */}
-
-                <div className="bg-[#00474b] text-white rounded-lg w-1/3 list-none">
-                  <input
-                    type="number"
-                    id="a60"
-                    name="amount"
-                    // value={50}
-                    // onChange={(e) => setTip(e.target.value)}
-                    placeholder="Custom"
-                    className="bg-[#f3f8fb] text-[#567979] rounded-lg  list-none"
-                  />
-                </div>
-              </div>
+            <div className="grid grid-cols-3 gap-3 mt-3">
+              {[5, 10, 15, 25, 50].map((tipValue) => (
+                <button
+                  key={tipValue}
+                  onClick={() => setTip(tipValue)}
+                  className={`py-3 rounded-md text-lg font-bold ${
+                    tip === tipValue
+                      ? "bg-[#26c2ad] text-white"
+                      : "bg-[#00474b] text-white"
+                  }`}
+                >
+                  {tipValue}%
+                </button>
+              ))}
+              <input
+                type="number"
+                onChange={(e) => setTip(e.target.value)}
+                className=" py-3 rounded-md bg-[#f3f9fa] text-[#00474b] font-bold text-center placeholder:text-[#7f9d9f]"
+                placeholder="Custom"
+              />
             </div>
           </div>
 
+          <div className="flex flex-col">
+            <div className="flex items-center justify-between">
           <label
             htmlFor=""
             className="text-[#748181] capitalize font-semibold py-5"
-          >
+            >
             Number of people
           </label>
+            {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+              </div>
+        <div className="relative">
           <input
             type="number"
             value={people}
-            onChange={(e) => setPeople(e.target.value)}
-            className="block bg-[#f3f8fb] px-5 py-1 justify-between"
-            placeholder="$"
-          />
-          {/* <Icon /> */}
+            onChange={handlePeopleChange}
+            className={`bg-[#f3f9fa] w-full px-10 py-3 rounded-md text-[#00474b] font-bold text-right ${
+                error ? "border border-red-500" : ""
+                }`}
+                placeholder="0"
+                />
+          <span className="absolute left-4 top-4 text-[#7f9d9f]">
+             <img src={Icon} alt="" />
+            </span>
+                </div>
         </div>
-
+        </div>
+      
         {/* right */}
-        <div className="w-full md:w-1/2 bg-[#00474b] text-white rounded-lg">
-          <div className="flex flex-col gap-5 p-5">
+        <div className="w-full md:w-1/2 bg-[#00474b] text-white rounded-xl p-6 flex flex-col justify-between">
+         <div>
             <div className="flex justify-between gap-5">
               <div>
                 <span>Tip Amount</span>
@@ -167,7 +126,7 @@ const Card = () => {
               </div>
 
               <span className="text-[#35b2a4] text-xl font-bold">
-                $ {tipValue}
+                ${tipAmountPerPerson.toFixed(2)}
               </span>
             </div>
 
@@ -178,14 +137,15 @@ const Card = () => {
               </div>
 
               <span className="text-[#35b2a4] text-xl font-bold">
-                ${perPersonShare}
+                ${totalAmountPerPerson.toFixed(2)}
               </span>
             </div>
+          </div>
 
-            <button className="bg-[#9fe8df] uppercase cursor-pointer transform hover:bg-[#7af3e5] text-[#0c514e] w-full sticky bottom-0 ">
+            <button onClick={handleReset} className="mt-6 bg-[#26c2ad] text-[#00474b] text-lg font-bold py-3 rounded-md uppercase hover:bg-[#9fe8df]"
+            >
               Reset
             </button>
-          </div>
         </div>
       </div>
     </>
